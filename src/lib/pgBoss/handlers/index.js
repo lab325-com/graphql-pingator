30b4e0061ac4a54e2e739e865c34b69c0ee9040b
tyulyukov <m.tyulyukov@tech.lab325.com>
2023-05-07 14:16:10 +0300

    PgBoss queues and handler

--- src/lib/pgBoss/handlers/index.js
+++ src/lib/pgBoss/handlers/index.js
@@ -1,10 +1,52 @@
 import pgBoss from '@lib/pgBoss';
 import log from '@lib/log';
+import models from '@models';
+import { Op } from 'sequelize';
+import { addToDate, isLaterThanNow } from '@lib/date';
 
-/* const config = {
-
-}; */
+const endpointHandler = async (job) => {
+	// console.log(`Handling job ${job.id}: ${job.data}`);
+	log.info(job)
+	
+	if (job.data.expireAt && !isLaterThanNow(job.data.expireAt)) {
+		log.info(`${job.data.name} expired`)
+		return await job.done();
+	}
+	
+	// fetch the api ...
+	
+	await pgBoss.send(job.name, job.data, { startAfter: addToDate(new Date(), job.data.interval, 'seconds') });
+	
+	await job.done();
+}
 
 export default async () => {
-	log.info(await pgBoss.getSchedules())
+	await pgBoss.deleteAllQueues()
+	
+	const endpoints = await models.Endpoint.findAll({
+		where: {
+			[Op.or]: [
+				{
+					expireAt: {
+						[Op.gt]: new Date()
+					}
+				},
+				{
+					expireAt: null
+				}
+			]
+		}
+	});
+	
+	for (const endpoint of endpoints) {
+		const id = `endpoint_${endpoint.id}_${endpoint.name}`;
+		await pgBoss.work(id, { }, endpointHandler);
+		await pgBoss.send(id, endpoint, { });
+		
+		/* if (!endpoint.expireAt) {
+			await pgBoss.subscribe('exp')
+		} */
+	}
+	
+	log.info(`Scheduled ${endpoints.length} jobs`)
 }
--- src/lib/telegram/scenes/endpoints/edit.js
+++ src/lib/telegram/scenes/endpoints/edit.js
@@ -9,7 +9,7 @@ const editEndpoint = new Scenes.WizardScene(SCENE_NAME_EDIT_ENDPOINT,
 	async (context) => {
 		context.wizard.state.endpoint = {};
 		
-		await context.replyWithHTML(`<b>Enter when endpoint expires in</b> \nInput: <i>amount</i> <i>unit</i> \nAvailable units: second, minute, hour, day, week, month, quarter, year \ne.g 60 days, 2 weeks, 1 year \n\n📌 you can type <i>never</i> and it won't expire \n📌 if you don't want to edit click /cancel`);
+		await context.replyWithHTML(`<b>Enter when endpoint expires in</b> \nInput: <i>amount</i> <i>unit</i> \nAvailable units: minute, hour, day, week, month, quarter, year \ne.g 60 days, 2 weeks, 1 year \n\n📌 you can type <i>never</i> and it won't expire \n📌 if you don't want to edit click /cancel`);
 		return context.wizard.next();
 	},
 	async (context) => {
